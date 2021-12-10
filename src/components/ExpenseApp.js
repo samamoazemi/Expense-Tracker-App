3425c6ace68e1fbd238c2d5d6d858503f7295b68
samamoazemi <moazemisama@gmail.com>
2021-12-10 00:25:19 -0800

    Update expense and income

--- src/components/ExpenseApp.js
+++ src/components/ExpenseApp.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import OverViewComponent from "./OverViewComponent";
 import TransActionComponent from "./TransActionComponent";
 
@@ -13,6 +13,17 @@ const ExpenseApp = () => {
         const data = {...formValues, id: Date.now() };
         setTransactions([...transactions, data]);
     }
+    
+    useEffect(() => {
+        let exp = 0;
+        let inc = 0;
+        transactions.forEach((t) => {
+            t.type ==="expense" ? (exp = exp + parseFloat(t.amount)) : (inc = inc + parseFloat(t.amount));
+        })
+        setExpense(exp);
+        setIncome(inc);
+    }, [transactions]);
+
     return (  
         <section className="container">
             <OverViewComponent 
